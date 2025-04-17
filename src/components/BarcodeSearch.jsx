821bb64f779bb9767e6fe180787b481676b90bb2
mohamed561 <mrajawi561@gmail.com>
2025-04-17 14:20:30 +0100

    easteregg

--- src/components/BarcodeSearch.jsx
+++ src/components/BarcodeSearch.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import JsBarcode from 'jsbarcode';
 import { database } from '../data/database';
 import { constantDatabase } from '../data/constantDatabase';
-import easterEggGif from '../assets/Blue And White Simple Big Game Football Facebook Post.png';
+import easterEggGif from '../assets/easterEgg.gif';
 
 const BarcodeSearch = () => {
   const [searchTerm, setSearchTerm] = useState('');
