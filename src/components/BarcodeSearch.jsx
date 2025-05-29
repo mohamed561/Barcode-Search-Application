57cd7297feb751411aa086b08ea49fc1f1ba5e4c
mohamed561 <mrajawi561@gmail.com>
2025-05-29 21:16:00 +0100

    updating db

--- src/components/BarcodeSearch.jsx
+++ src/components/BarcodeSearch.jsx
@@ -629,7 +629,7 @@ const BarcodeSearch = () => {
     Built with <span style={{ color: "#e25555" }}>❤️</span> by <strong>Wyatt</strong> · <strong>Team AINSBAA</strong>
   </p>
   <p style={styles.footerText}>
-    DB Snapshot: <strong>2025-05-13</strong>
+    DB Snapshot: <strong>2025-05-24</strong>
   </p>
   <p style={styles.footerText}>
     <strong>Version 2.1</strong>
