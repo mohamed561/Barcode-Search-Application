f8082609519d7a8c32d28fd3c31000fa16094dda
mohamed561 <mrajawi561@gmail.com>
2025-08-06 22:53:30 +0100

    gg

--- src/components/BarcodeSearch.jsx
+++ src/components/BarcodeSearch.jsx
@@ -729,10 +729,10 @@ const BarcodeSearch = () => {
             Built with <span style={{ color: "#e25555" }}>❤️</span> by <strong>Wyatt</strong> · <strong>Team AINSBAA</strong>
           </p>
           <p style={styles.footerText}>
-            DB Snapshot: <strong>2025-07-28</strong>
+            DB Snapshot: <strong>2025-08-06</strong>
           </p>
           <p style={styles.footerText}>
-            <strong>Version 3.0</strong>
+            <strong>Version 3.0.1</strong>
           </p>
         </div>
       </div>
